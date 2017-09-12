33c7e6b8086cce0a07e447eb2d402941f0520fa1
moonjuhyeon <hoop_ster@naver.com>
2017-09-12 10:35:08 +0900

    s

--- WebContent/assets/js/common.js
+++
@@ -1,8 +0,0 @@
-var slideLeft = new Menu({wrapper: '#wrapper', type: 'slide-left', menuOpenerClass: '.c-button', maskId: '#c-mask'});
-
-var slideLeftBtn = document.querySelector('#c-button--slide-left');
-
-slideLeftBtn.addEventListener('click', function(e) {
-  e.preventDefault;
-  slideLeft.open();
-});
