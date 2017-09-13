035b6aa43c561f5b4a2d4bb6a17a4c7aa0921164
moonjuhyeon <hoop_ster@naver.com>
2017-09-13 21:46:40 +0900

    fixfixfix

--- src/com/sports/dto/QADTO.java
+++ src/com/sports/dto/QADTO.java
@@ -2,158 +2,84 @@ package com.sports.dto;
 
 public class QADTO {
 
-	private String qa_no; 
-	private String q_no;
-	private String answer_yn;
+	private String qa_no;
+	private String answer;
+	private String user_no;
 	private String secret_yn;
-	private String new_yn;
-	private String title;	
-	private String contents; 
-	private String file_path;	
-	private String file_name;	
-	private String reg_user_no;	
-	private String reg_dt; 
-	private String chg_user_no; 
-	private String chg_dt; 
-	private String read_more;
-	private String user_name;
-	private String[] allCheckSeq;
-	private String search; 
+	private String title;
+	private String contents;
+	private String reg_user_no;
+	private String reg_dt;
+	private String chg_user_no;
+	private String chg_dt;
+	public QADTO(){}
 	
-	public String getNew_yn() {
-		return new_yn;
-	}
-
-	public void setNew_yn(String new_yn) {
-		this.new_yn = new_yn;
-	}
-	
-	public String getUser_name() {
-		return user_name;
-	}
-
-	public void setUser_name(String user_name) {
-		this.user_name = user_name;
+	public QADTO(String user_no, String secret_yn, String title, String contents, String reg_user_no){
+		this.user_no = user_no;
+		this.secret_yn = secret_yn;
+		this.title = title;
+		this.contents = contents;
+		this.reg_user_no = reg_user_no;
 	}
 	
 	public String getQa_no() {
 		return qa_no;
 	}
-	
 	public void setQa_no(String qa_no) {
 		this.qa_no = qa_no;
 	}
-	
-	public String getQ_no() {
-		return q_no;
+	public String getAnswer() {
+		return answer;
 	}
-	
-	public void setQ_no(String q_no) {
-		this.q_no = q_no;
+	public void setAnswer(String answer) {
+		this.answer = answer;
 	}
-	
-	public String getAnswer_yn() {
-		return answer_yn;
+	public String getUser_no() {
+		return user_no;
 	}
-	
-	public void setAnswer_yn(String answer_yn) {
-		this.answer_yn = answer_yn;
+	public void setUser_no(String user_no) {
+		this.user_no = user_no;
 	}
-	
 	public String getSecret_yn() {
 		return secret_yn;
 	}
-	
 	public void setSecret_yn(String secret_yn) {
 		this.secret_yn = secret_yn;
 	}
-	
 	public String getTitle() {
 		return title;
 	}
-	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
 	public String getContents() {
 		return contents;
 	}
-	
 	public void setContents(String contents) {
 		this.contents = contents;
 	}
-	
-	public String getFile_path() {
-		return file_path;
-	}
-	
-	public void setFile_path(String file_path) {
-		this.file_path = file_path;
-	}
-	
-	public String getFile_name() {
-		return file_name;
-	}
-	
-	public void setFile_name(String file_name) {
-		this.file_name = file_name;
-	}
-	
 	public String getReg_user_no() {
 		return reg_user_no;
 	}
-	
 	public void setReg_user_no(String reg_user_no) {
 		this.reg_user_no = reg_user_no;
 	}
-	
 	public String getReg_dt() {
 		return reg_dt;
 	}
-	
 	public void setReg_dt(String reg_dt) {
 		this.reg_dt = reg_dt;
 	}
-	
 	public String getChg_user_no() {
 		return chg_user_no;
 	}
-	
 	public void setChg_user_no(String chg_user_no) {
 		this.chg_user_no = chg_user_no;
 	}
-	
 	public String getChg_dt() {
 		return chg_dt;
 	}
-	
 	public void setChg_dt(String chg_dt) {
 		this.chg_dt = chg_dt;
 	}
-	
-	public String getRead_more() {
-		return read_more;
-	}
-	
-	public void setRead_more(String read_more) {
-		this.read_more = read_more;
-	}
-	
-	public String[] getAllCheckSeq() {
-		return allCheckSeq;
-	}
-
-	public void setAllCheckSeq(String[] allCheckSeq) {
-		this.allCheckSeq = allCheckSeq;
-	}
-	
-	public String getSearch() {
-		return search;
-	}
-
-	public void setSearch(String search) {
-		this.search = search;
-	}
-	
 }
--- src/com/sports/util/MailUtil.java
+++ src/com/sports/util/MailUtil.java
@@ -14,19 +14,25 @@ import javax.mail.internet.MimeMessage;
 
 public class MailUtil {
 	public static void sendMail(String recipient, String subject, String body) throws AddressException, MessagingException {
-		String host = "gmail-smtp.l.google.com";
+		// 네이버일 경우 smtp.naver.com 을 입력합니다.
+		// Google일 경우 smtp.gmail.com 을 입력합니다.
+		String host = "smtp.naver.com";
 
-		final String username = "kangseokopo@gmail.com"; 
-		final String password = "vhfflxpr"; 
+		// 메일 관련 정보
+		final String username = "portfolio_jg"; // 네이버 아이디 @naver.com은 입력생략
+		final String password = "123qweasd"; // 네이버 이메일 비밀번호 입력
+		int port = 465; // 포트번호
 
-		Properties props = System.getProperties(); 
+		Properties props = System.getProperties(); // 정보를 담기 위한 객체 생성
 
-		  props.put("mail.transport.protocol", "smtp");
-	      props.put("mail.smtp.starttls.enable","true");
-	      props.put("mail.smtp.host", host);
-	      props.put("mail.smtp.auth", "true");
+		// SMTP 서버 정보 설정
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", port);
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.ssl.enable", "true");
+		props.put("mail.smtp.ssl.trust", host);
 
-
+		// Session 생성
 		Session session = Session.getDefaultInstance(props, new Authenticator() {
 			String un = username;
 			String pw = password;
@@ -35,14 +41,18 @@ public class MailUtil {
 				return new PasswordAuthentication(un, pw);
 			}
 		});
-		session.setDebug(true); 
+		session.setDebug(true); // for debug
 
-		Message mimeMessage = new MimeMessage(session); 
-		mimeMessage.setFrom(new InternetAddress("kangseokopo@gmail.com"));
+		Message mimeMessage = new MimeMessage(session); // MimeMessage 생성
+		mimeMessage.setFrom(new InternetAddress("portfolio_jg@naver.com"));
+		// 발신자 셋팅, 보내는 사람의 이메일주소를 한번 더 입력합니다.
+		// 이메일 풀 주소를 다 작성해주세요.
 		mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+		// 수신자셋팅
+		// .TO외에 .CC(참조) .BCC(숨은참조)도 있음
 
-		mimeMessage.setSubject(subject); 
-		mimeMessage.setText(body); 
-		Transport.send(mimeMessage); 
+		mimeMessage.setSubject(subject); // 제목셋팅
+		mimeMessage.setText(body); // 내용세팅
+		Transport.send(mimeMessage); // javax.mail.Transport.send() 이용
 	}
 }
