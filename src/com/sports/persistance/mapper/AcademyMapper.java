104d14807638b4be2b77214afbc1995466a0c5a5
DATA16-4 <DATA16-4@DATA16-4-HP>
2017-09-20 21:34:11 +0900

    aaa

--- src/com/sports/persistance/mapper/AcademyMapper.java
+++ src/com/sports/persistance/mapper/AcademyMapper.java
@@ -1,8 +1,11 @@
 package com.sports.persistance.mapper;
 
 import com.sports.config.Mapper;
+import com.sports.dto.AcademyDTO;
 
 @Mapper("AcademyMapper")
 public interface AcademyMapper {
+
+	void insertAcademy(AcademyDTO aDTO);
 
 }
