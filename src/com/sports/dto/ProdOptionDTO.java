d545e3e9d1a2b6f70f70278f7ccfc7a0c927648e
Data3811-36 <Data3811-36@Data3811-36-PC>
2017-09-25 19:12:12 +0900

    zdfdd

---
+++ src/com/sports/dto/ProdOptionDTO.java
@@ -0,0 +1,49 @@
+package com.sports.dto;
+
+public class ProdOptionDTO {
+	
+	private String prod_no;
+	private String prod_name;
+	private String code_name;
+	private String opt_name;
+	private String opt_price;
+	private String opt_no;
+	
+	public String getOpt_no() {
+		return opt_no;
+	}
+	public void setOpt_no(String opt_no) {
+		this.opt_no = opt_no;
+	}
+	public String getProd_no() {
+		return prod_no;
+	}
+	public void setProd_no(String prod_no) {
+		this.prod_no = prod_no;
+	}
+	public String getProd_name() {
+		return prod_name;
+	}
+	public void setProd_name(String prod_name) {
+		this.prod_name = prod_name;
+	}
+	public String getCode_name() {
+		return code_name;
+	}
+	public void setCode_name(String code_name) {
+		this.code_name = code_name;
+	}
+	public String getOpt_name() {
+		return opt_name;
+	}
+	public void setOpt_name(String opt_name) {
+		this.opt_name = opt_name;
+	}
+	public String getOpt_price() {
+		return opt_price;
+	}
+	public void setOpt_price(String opt_price) {
+		this.opt_price = opt_price;
+	}
+	
+}
